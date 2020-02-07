258786838e5e159b3ee3799e83ced7755fac445e
Adnan Can Turkay <acturkay@gmail.com>
2020-02-07 14:32:49 -0600

    small push

--- Project1/main.py
+++ Project1/main.py
@@ -10,15 +10,9 @@ data = json.load(file)
 awards = ["Best Motion Picture", "Best Performance by an Actress", "Best Performance by an Actor",
           "Best Performance by an Actress in a Supporting Role", "Best Performance by an Actor in a Supporting Role",
           "Best Director", "Best Screenplay", "Best Original Score", "Best Original Song", "Best Television Series"]
-awards = [a.lower() for a in awards]
+#awards = [a.lower() for a in awards]
 
 categories = ["Drama", "Musical or Comedy", "Animated", "Foreign Language"]
-categories = [c.lower() for c in categories]
+#categories = [c.lower() for c in categories]
 
-for tweet in data:
-    text = tweet["text"].lower()
-    sentence = text.split()
-    if "won" in sentence:
-        for award in awards:
-            if award in text:
-                print(text)
+
