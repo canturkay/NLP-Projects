c6bc31df9f88125b68a1425a5a64030b3fc7eeb7
Adnan Can Turkay <acturkay@gmail.com>
2020-02-08 16:00:53 -0600

    Update main.py

--- Project1/main.py
+++ Project1/main.py
@@ -1,18 +1,7 @@
-import json
-from levenshtein import fuzzy_match
-from host import get_hosts
-#check timestamps
-
-file = open('data/gg2020.json')
-
-data = json.load(file)
-
-awards = ["Best Motion Picture", "Best Performance by an Actress", "Best Performance by an Actor",
-          "Best Performance by an Actress in a Supporting Role", "Best Performance by an Actor in a Supporting Role",
-          "Best Director", "Best Screenplay", "Best Original Score", "Best Original Song", "Best Television Series"]
-#awards = [a.lower() for a in awards]
-
-categories = ["Drama", "Musical or Comedy", "Animated", "Foreign Language"]
-#categories = [c.lower() for c in categories]
+from .get_nominee_names import get_nominee_names
+class response:
 
 
+    def __init__(self):
+    def get_nominees(self):
+        get_nominee_names()
