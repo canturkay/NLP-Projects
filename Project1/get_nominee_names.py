97901d6fe401bc7c592c40051063c017a9f6e47a
Adnan Can Turkay <acturkay@gmail.com>
2020-02-08 15:11:28 -0600

    common first names

--- Project1/get_nominee_names.py
+++ Project1/get_nominee_names.py
@@ -10,7 +10,7 @@ def get_nominee_names(path, awards_list):
     file = open(path)
     data = json.load(file)
 
-    file_first_names = open('data/first_names.json')
+    file_first_names = open('data/names.json')
     first_names = json.load(file_first_names)
 
     stopwords = ['RT', 'Golden', 'Globes', 'GoldenGlobes', '@goldenglobes', '@']
@@ -19,23 +19,23 @@ def get_nominee_names(path, awards_list):
     count = 0
 
     for tweet in data:
-        if 'nomin' in tweet:
+        if 'nomin' in tweet.lower():
           tags = nltk.pos_tag(nltk.word_tokenize(tweet))
           for i in range(len(tags) - 1):
             if tags[i][1] == 'NNP' and tags[i][0] not in stopwords:
                 name = tags[i][0]
                 if tags[i + 1][1] == 'NNP' and tags[i + 1][0] not in stopwords:
                     last_name = tags[i + 1][0]
-                    if name in first_names:
-                        inc = 5
-                    else:
-                        inc = 1
                     for award in awards_list:
                         if award in tweet:
                             if name not in award and last_name not in award:
                                 if name + ' ' + last_name in potential_names:
-                                    potential_names[name + ' ' + last_name][1] += inc
+                                    potential_names[name + ' ' + last_name][1] += 1
                                 else:
+                                    if name in first_names:
+                                        inc = 10
+                                    else:
+                                        inc = 1
                                     potential_names[name + ' ' + last_name] = [award, inc]
 
         count += 1
@@ -44,10 +44,10 @@ def get_nominee_names(path, awards_list):
 
     filtered_potential_names = []
     print(potential_names)
-    threshold = 5/1000000
+    threshold = 1/100000
 
     for name, award_and_count in potential_names.items():
-        if award_and_count[1] > threshold * len(data):
+        if award_and_count[1] > 11:  # threshold * len(data):
             filtered_potential_names.append(name + ' - ' + award_and_count[0])
 
     nominees = filtered_potential_names
