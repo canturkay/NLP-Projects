3a57d6caaee029bfab16d50f51ef6858c8ae4d92
Clarissa Qian <clarissaqianx@gmail.com>
2020-02-08 16:30:34 -0600

    change nominee film format

--- Project1/nominees.py
+++ Project1/nominees.py
@@ -1,23 +1,11 @@
 import json
-import spacy
-import wikipedia
 import requests
 from bs4 import BeautifulSoup
 import re
 from imdb import IMDb
-import time
-import numpy as np
-import pandas as pd
 
-year = '2015'
-def all_movies():
-    '''
-    df = pd.read_csv('data.tsv', sep='\t', low_memory=False)
-    df.query('titleType == "movie" and startYear == "2014"', inplace=True)
-    all_films = set(df['primaryTitle'])
-    return all_films
-    '''
 
+def all_movies(year):
     film_titles = set()
     my_regex = "\(.*\)|\s-\s.*"
     nationalities = ['American', 'British']
@@ -41,11 +29,9 @@ def all_movies():
     return film_titles
 
 
-all_movies()
 dramas = set()
 comedies = set()
 anim = set()
-ia = IMDb()
 
 
 def category(film_title):
@@ -56,10 +42,10 @@ def category(film_title):
     elif film_title in anim:
         return 'Animation'
     # print(dramas, comedies, film_title)
+    ia = IMDb()
     movies = ia.search_movie(film_title)
     for m in movies:
         if m['title'] == film_title or m['title'][:len(film_title)] == film_title:
-            # if 'year' not in m or m['year'] == str(int(year)-1):
             movie = ia.get_movie(m.getID())
             if 'Musical' in movie['genres']:
                 comedies.add(film_title)
@@ -78,51 +64,54 @@ def category(film_title):
             return 'Comedy'
 
 
-file = open('data/gg2015.json')
+def get_nominee_films(data, year):
+    keywords = ['nomin']
+    noms = [tweet['text'] for tweet in data if all(w in tweet['text'] for w in keywords)]
 
-data = json.load(file)
+    all_films = all_movies(year)
+    drama_films = {}
+    com_films = {}
+    anim_films = {}
+    for n in noms:
+        if not any(w in n for w in ['song', 'foreign', 'tv', 'show', 'series', 'actress', 'actor',
+                                    'supporting', 'original', 'performance',
+                                    'score', 'director', 'television']):
+            films = [f for f in all_films if f in n]
+            for film in films:
+                c = category(film)
+                if c == 'Drama':
+                    if film in drama_films:
+                        drama_films[film] += 1
+                    else:
+                        drama_films[film] = 1
+                elif c == 'Comedy':
+                    if film in com_films:
+                        com_films[film] += 1
+                    else:
+                        com_films[film] = 1
+                elif c == 'Animation':
+                    if film in anim_films:
+                        anim_films[film] += 1
+                    else:
+                        anim_films[film] = 1
+    return sorted(drama_films, key=drama_films.get, reverse=True)[:6], \
+           sorted(com_films, key=com_films.get, reverse=True)[:6],\
+           sorted(anim_films, key=anim_films.get, reverse=True)[:6]
 
-awards = ["Best Motion Picture", "Best Performance by an Actress", "Best Performance by an Actor",
-          "Best Performance by an Actress in a Supporting Role", "Best Performance by an Actor in a Supporting Role",
-          "Best Director", "Best Screenplay", "Best Original Score", "Best Original Song", "Best Television Series"]
-#awards = [a.lower() for a in awards]
+# file = open('data/gg2013.json')
+# data = json.load(file)
 
-categories = ["Drama", "Musical or Comedy", "Animated", "Foreign Language"]
-#categories = [c.lower() for c in categories]
+data = list()
 
-keywords = ['nomin']  #, 'best']
-noms = [tweet['text'] for tweet in data if all(w in tweet['text'] for w in keywords)]
+with open('data/gg2020.json', 'r') as f_in:
+    for line in f_in:
+        data.append(json.loads(line))
 
-all_films = all_movies()
-drama_films = {}
-com_films = {}
-anim_films = {}
-for n in noms:
-    if not any(w in n for w in ['song', 'foreign', 'tv', 'show', 'series', 'actress', 'actor',
-                                'supporting', 'original', 'performance',
-                                'score', 'director', 'television']):
-        films = [f for f in all_films if f in n]
-        for film in films:
-            c = category(film)
-            if c == 'Drama':
-                if film in drama_films:
-                    drama_films[film] += 1
-                else:
-                    drama_films[film] = 1
-            elif c == 'Comedy':
-                if film in com_films:
-                    com_films[film] += 1
-                else:
-                    com_films[film] = 1
-            elif c == 'Animation':
-                if film in anim_films:
-                    anim_films[film] += 1
-                else:
-                    anim_films[film] = 1
+nominees = get_nominee_films(data, '2020')
 print("Best Motion Picture - Drama")
-print(sorted(drama_films, key=drama_films.get, reverse=True)[:6])
+print(nominees[0])
 print("Best Motion Picture - Comedy or Musical")
-print(sorted(com_films, key=com_films.get, reverse=True)[:6])
+print(nominees[1])
 print("Best Animated Film")
-print(sorted(anim_films, key=anim_films.get, reverse=True)[:6])
+print(nominees[2])
 
