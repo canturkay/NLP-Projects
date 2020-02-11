d198f03c10305d352f39ec3a11628cccc7961848
ahkaneti <aaronkaneti2020@u.northwestern.edu>
2020-02-10 20:48:01 -0600

    Awards created

--- Project1/regex.py
+++ Project1/regex.py
@@ -1,111 +1,109 @@
 import re
 awards_regex = {"best screenplay - motion picture":
-                    r'.*best screenplay.*motion picture',
+                    r'best screenplay',
                 "best director - motion picture":
-                    r'best director.*motion picture',
+                    r'best director',
                 "best performance by an actress in a television series - comedy or musical":
-                    r'best performance.*actress.*(tv|television) series.*comedy.*musical',
+                    r'best.*actress.*(tv|television|series).*comedy',
                 "best foreign language film":
                     r'best foreign language',
                 "best performance by an actor in a supporting role in a motion picture":
-                    r'best performance.*actor.*supporting.*motion picture',
+                    r'best.*supporting.*actor.*(picture|movie|)',
                 "best performance by an actress in a supporting role in a series, mini-series or motion picture made for television":
-                    r'best performance.*actress.*supporting.*series.*motion picture',
+                    r'best.*supporting.*actress.*series',
                 "best motion picture - comedy or musical":
-                    r'best motion picture .*comedy.*musical',
+                    r'best.*(picture|movie) .*comedy',
                 "best mini-series or motion picture made for television":
-                    r'best mini.*series.*(tv|television)',
+                    r'best.*(limited|mini).*series',
                 "best original score - motion picture":
-                    r'best original score.*motion picture',
+                    r'best original score',
                 "best performance by an actress in a television series - drama":
-                    r'best performance.*actress.*(tv|television).*drama',
+                    r'best.*actress.*(tv|television).*drama',
                 "best performance by an actress in a motion picture - drama":
-                    r'bet performance.*actress,*motion picture.*drama',
+                    r'best.*actress.*drama',
                 "best performance by an actor in a motion picture - comedy or musical":
-                    r'best performance.*actor.*motion picture.*comedy.*musical',
+                    r'best.*actor.*comedy',
                 "best motion picture - drama":
-                    r'best motion picture.*drama',
+                    r'best.*drama',
                 "best performance by an actor in a supporting role in a series, mini-series or motion picture made for television":
-                    r'best performance.*actor.*supporting.*series.*motion picture.*made for (tv|television)',
+                    r'best.*supporting.*actor.*(series|picture).*(tv|television)',
                 "best performance by an actress in a supporting role in a motion picture":
-                    r'best performance.*actress.*supporting.*motion picture',
+                    r'best.*supporting.*actress',
                 "best television series - drama":
-                    r'best [tv|television] series.*drama',
+                    r'best.*drama.*series',
                 "best performance by an actor in a mini-series or motion picture made for television":
-                    r'best performance.*actor.*series.*motion picture.*made for(tv|television)',
+                    r'best.*actor.*(tv|television)',
                 "best performance by an actress in a mini-series or motion picture made for television":
-                    r'best performance.*actress.*series.*motion picture.*made for (tv|television)',
+                    r'best.*actress.*(tv|television)',
                 "best animated feature film":
-                    r'best animated feature film',
+                    r'best animated',
                 "best original song - motion picture":
-                    r'best original song.*motion picture',
+                    r'best original song',
                 "best performance by an actor in a motion picture - drama":
-                    r'best performance.*actor.*motion picture.*drama',
+                    r'best.*actor.*drama',
                 "best television series - comedy or musical":
-                    r'best [tv|television] series.*comedy.*musical',
+                    r'best (tv|television).*comedy',
                 "best performance by an actor in a television series - drama":
-                    r'best performance.*actor.*(tv|television) series.*drama',
+                    r'best.*actor.*(tv|television).*drama',
                 "best performance by an actor in a television series - comedy or musical":
-                    r'best performance.*actor.*(tv|television) series.*comedy.*musical'
+                    r'best.*actor.*(tv|television).*comedy',
+                "cecil b. demille award":
+                r'(cecil|lifetime).*award'
                 }
 
 person_awards_regex = {"best screenplay - motion picture":
-                    r'.*best screenplay.*motion picture',
-                "best director - motion picture":
-                    r'best director.*motion picture',
-                "best performance by an actress in a television series - comedy or musical":
-                    r'best performance.*actress.*(tv|television) series.*comedy.*musical',
-                "best performance by an actor in a supporting role in a motion picture":
-                    r'best performance.*actor.*supporting.*motion picture',
-                "best performance by an actress in a supporting role in a series, mini-series or motion picture made for television":
-                    r'best performance.*actress.*supporting.*series.*motion picture',
-                "best original score - motion picture":
-                    r'best original score.*motion picture',
-                "best performance by an actress in a television series - drama":
-                    r'best performance.*actress.*[tv|television].*drama',
-                "best performance by an actress in a motion picture - drama":
-                    r'bet performance.*actress,*motion picture.*drama',
-                "best performance by an actor in a motion picture - comedy or musical":
-                    r'best performance.*actor.*motion picture.*comedy.*musical',
-                "best performance by an actor in a supporting role in a series, mini-series or motion picture made for television":
-                    r'best performance.*actor.*supporting.*series.*motion picture.*made for [tv|television]',
-                "best performance by an actress in a supporting role in a motion picture":
-                    r'best performance.*actress.*supporting.*motion picture',
-                "best performance by an actor in a mini-series or motion picture made for television":
-                    r'best performance.*actor.*series.*motion picture.*made for[tv|television]',
-                "best performance by an actress in a mini-series or motion picture made for television":
-                    r'best performance.*actress.*series.*motion picture.*made for [tv|television]',
-                "best original song - motion picture":
-                    r'best original song.*motion picture',
-                "best performance by an actor in a motion picture - drama":
-                    r'best performance.*actor.*motion picture.*drama',
-                "best performance by an actor in a television series - drama":
-                    r'best performance.*actor.*[tv|television] series.*drama',
-                "best performance by an actor in a television series - comedy or musical":
-                    r'best performance.*actor.*[tv|television] series.*comedy.*musical'
-                }
+                       r'best screenplay',
+                       "best director - motion picture":
+                       r'best director',
+                       "best performance by an actress in a television series - comedy or musical":
+                       r'best.*actress.*(tv|television|series).*comedy', 
+                       "best performance by an actor in a supporting role in a motion picture":
+                       r'best.*supporting.*actor.*(picture|movie|)',
+                       "best performance by an actress in a supporting role in a series, mini-series or motion picture made for television":
+                       r'best.*supporting.*actress.*series', 
+                       "best performance by an actress in a television series - drama":
+                       r'best.*actress.*(tv|television).*drama',
+                       "best performance by an actress in a motion picture - drama":
+                       r'best.*actress.*drama',
+                       "best performance by an actor in a motion picture - comedy or musical":
+                       r'best.*actor.*comedy',
+                       "best performance by an actor in a supporting role in a series, mini-series or motion picture made for television":
+                       r'best.*supporting.*actor.*(series|picture).*(tv|television)',
+                       "best performance by an actress in a supporting role in a motion picture":
+                       r'best.*supporting.*actress',
+                       "best performance by an actor in a mini-series or motion picture made for television":
+                       r'best.*actor.*(tv|television)',
+                       "best performance by an actress in a mini-series or motion picture made for television":
+                       r'best.*actress.*(tv|television)',
+                       "best performance by an actor in a motion picture - drama":
+                       r'best.*actor.*drama',
+                       "best performance by an actor in a television series - drama":
+                       r'best.*actor.*(tv|television).*drama',
+                       "best performance by an actor in a television series - comedy or musical":
+                       r'best.*actor.*(tv|television).*comedy', 
+                       "cecil b. demille award":
+                       r'(cecil|lifetime).*award',
+                       }
 
-movie_awards_regex = {"best screenplay - motion picture":
-                    r'.*best screenplay.*motion picture',
-                "best foreign language film":
-                    r'best foreign language',
-                "best motion picture - comedy or musical":
-                    r'best motion picture .*comedy.*musical',
-                "best mini-series or motion picture made for television":
-                    r'best mini.*series.*(tv|television)',
-                "best original score - motion picture":
-                    r'best original score.*motion picture',
-                "best motion picture - drama":
-                    r'best motion picture.*drama',
-                "best television series - drama":
-                    r'best [tv|television] series.*drama',
-                "best animated feature film":
-                    r'best animated feature film',
-                "best original song - motion picture":
-                    r'best original song.*motion picture',
-                "best television series - comedy or musical":
-                    r'best [tv|television] series.*comedy.*musical',
-                }
+movie_awards_regex = {"best foreign language film":
+                      r'best foreign language', 
+                      "best original score - motion picture":
+                      r'best original score',
+                      "best motion picture - comedy or musical":
+                      r'best.*(picture|movie) .*comedy',
+                      "best mini-series or motion picture made for television":
+                      r'best.*(limited|mini).*series',
+                      "best motion picture - drama":
+                      r'best.*drama', 
+                      "best television series - drama":
+                      r'best.*drama.*series', 
+                      "best television series - comedy or musical":
+                      r'best (tv|television).*comedy',
+                      "best original song - motion picture":
+                      r'best original song',
+                      "best animated feature film":
+                      r'best animated',
+                      }
 
 for key in awards_regex.keys():
     awards_regex[key] = re.compile(awards_regex[key])
